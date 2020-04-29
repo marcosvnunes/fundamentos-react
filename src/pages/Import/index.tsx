53dc12bb9879a4a75c6c042dc3295caaa898f104
Marcos <marcosnunescomercial@gmail.com>
2020-04-29 18:22:47 -0300

    challenge-finalized

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(async uploadedFile => {
+      data.append('file', uploadedFile.file, uploadedFile.name);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
-    }
+      try {
+        await api.post('/transactions/import', data);
+        history.push('/');
+      } catch (err) {
+        console.log(err.response.error);
+      }
+    });
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadFiles: FileProps[] = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+    setUploadedFiles(uploadFiles);
   }
 
   return (
